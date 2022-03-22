fadd71f4c66cc7844be4217c6f52b1fde7b4e965
Ruila <4momolmr@gmail.com>
2022-03-22 21:58:29 +0800

    fix eslint and prettier

---
+++ src/pages/demo.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Counter } from "../components/Counter"
+
+const DemoPage = (): JSX.Element => {
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default DemoPage
